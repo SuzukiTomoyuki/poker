febeec713be9d53bd70a31f4f7d8fa10e0d7fe94
SuzukiTomoyuki <t.suzuki@ashita-team.com>
2018-04-17 14:23:27 +0900

    分かりにくい名前をリファクタリング

--- poker_three.rb
+++ poker_three.rb
@@ -2,34 +2,34 @@ class PokerThree
   OPEN_RESULT = { "win"=>1, "draw"=>0, "lose"=>-1}
   HAND_STRONGTH_LIST = { "straight_flash"=>0, "three_of_a_kind"=>1, "straight"=>2, "flash"=>3, "pair"=>4, "highcard"=>5 }
   
-  def open(player1_hand, player2_hand)
-    player1_hand_strongth = hand_strongth(player1_hand)
-    player2_hand_strongth = hand_strongth(player2_hand)
+  def open(player1_cards, player2_cards)
+    player1_hand_strongth = hand_strongth(player1_cards)
+    player2_hand_strongth = hand_strongth(player2_cards)
     if (player1_hand_strongth < player2_hand_strongth)
       OPEN_RESULT["win"]
     elsif (player1_hand_strongth == player2_hand_strongth)
-      check_same_hand_strongth_conditions(player1_hand, player1_hand_strongth, player2_hand, player2_hand_strongth)
+      check_same_hand_strongth_conditions(player1_cards, player1_hand_strongth, player2_cards, player2_hand_strongth)
     else
       OPEN_RESULT["lose"]
     end
   end
   
 
-  def check_same_hand_strongth_conditions(player1, player1_hand, player2, player2_hand)
-    if (player1_hand == HAND_STRONGTH_LIST["straight_flash"] || player1_hand == HAND_STRONGTH_LIST["straight"])
-      check_straight_hand(player1.hand_rank, player2.hand_rank)
-    elsif (player1_hand == HAND_STRONGTH_LIST["three_of_a_kind"])
-      check_three_of_a_kind(player1.hand_rank[0], player2.hand_rank[0])
-    elsif (player1_hand == HAND_STRONGTH_LIST["flash"] || player1_hand == HAND_STRONGTH_LIST["highcard"])
-      check_flash_high_hand(player1.hand_rank, player2.hand_rank)
-    elsif (player1_hand == HAND_STRONGTH_LIST["pair"])
-      check_pair_hand(player1.hand_rank, player2.hand_rank)
+  def check_same_hand_strongth_conditions(player1_cards, player1_hand_strongth, player2_cards, player2_hand_strongth)
+    if (player1_hand_strongth == HAND_STRONGTH_LIST["straight_flash"] || player1_hand_strongth == HAND_STRONGTH_LIST["straight"])
+      check_straight_hand(player1_cards.hand_rank, player2_cards.hand_rank)
+    elsif (player1_hand_strongth == HAND_STRONGTH_LIST["three_of_a_kind"])
+      check_three_of_a_kind(player1_cards.hand_rank[0], player2_cards.hand_rank[0])
+    elsif (player1_hand_strongth == HAND_STRONGTH_LIST["flash"] || player1_hand_strongth == HAND_STRONGTH_LIST["highcard"])
+      check_flash_high_hand(player1_cards.hand_rank, player2_cards.hand_rank)
+    elsif (player1_hand_strongth == HAND_STRONGTH_LIST["pair"])
+      check_pair_hand(player1_cards.hand_rank, player2_cards.hand_rank)
     end
   end
 
-  def check_straight_hand(player1_hand, player2_hand)
-    player1_strongth = straight_strongth(player1_hand)
-    player2_strongth = straight_strongth(player2_hand)
+  def check_straight_hand(player1_cards, player2_cards)
+    player1_strongth = straight_strongth(player1_cards)
+    player2_strongth = straight_strongth(player2_cards)
     if (player1_strongth > player2_strongth)
       OPEN_RESULT["win"]
     elsif (player1_strongth < player2_strongth)
@@ -39,11 +39,11 @@ class PokerThree
     end
   end
 
-  def straight_strongth(player_hand)
-    if ((player_hand.min - player_hand.max).abs == 12 and player_hand.include?(1))
+  def straight_strongth(player_card_ranks)
+    if ((player_card_ranks.min - player_card_ranks.max).abs == 12 and player_card_ranks.include?(1))
       return 14
     else
-      return player_hand.max
+      return player_card_ranks.max
     end
   end
 
@@ -61,32 +61,31 @@ class PokerThree
     end
   end
 
-  def check_flash_high_hand(player1_hand, player2_hand)     # rankですよね？
-    # a.dupでもディープコピーできる
-    player1_hand_temp = Marshal.load(Marshal.dump(player1_hand))
-    player2_hand_temp = Marshal.load(Marshal.dump(player2_hand))
-    player1_strongth, player1_rank = get_rank_strongth(player1_hand_temp)
-    player2_strongth, player2_rank = get_rank_strongth(player2_hand_temp)
+  def check_flash_high_hand(player1_card_ranks, player2_card_ranks)
+    player1_card_ranks_temp = player1_card_ranks.dup
+    player2_card_ranks_temp = player2_card_ranks.dup
+    player1_strongth, player1_rank = rank_strongth(player1_card_ranks_temp)
+    player2_strongth, player2_rank = rank_strongth(player2_card_ranks_temp)
     if (player1_strongth > player2_strongth)
       OPEN_RESULT["win"]
     elsif (player1_strongth < player2_strongth)
       OPEN_RESULT["lose"]
     elsif (player1_strongth == player2_strongth)
-      if (player1_hand_temp.length == 1)
+      if (player1_card_ranks_temp.length == 1)
         OPEN_RESULT["draw"]
       else
-        player1_hand_temp.delete(player1_rank)
-        player2_hand_temp.delete(player2_rank)
-        check_flash_high_hand(player1_hand_temp, player2_hand_temp)
+        player1_card_ranks_temp.delete(player1_rank)
+        player2_card_ranks_temp.delete(player2_rank)
+        check_flash_high_hand(player1_card_ranks_temp, player2_card_ranks_temp)
       end
     end
   end
 
-  def get_rank_strongth(player_hand)
-    if (player_hand.min == 1)
+  def rank_strongth(player_card_ranks)
+    if (player_card_ranks.min == 1)
       return 14,1
     else
-      return player_hand.max, player_hand.max
+      return player_card_ranks.max, player_card_ranks.max
     end
   end
 
@@ -123,6 +122,3 @@ class PokerThree
     end
   end
 end
-
-
-# cards_ranks
